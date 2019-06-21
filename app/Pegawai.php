50927f83d59db8e71e3bee78ef0c02ac7d271388
Levana199 <alvivalen@gmail.com>
2019-06-21 08:35:38 +0700

    Modify model pegawai

--- app/Pegawai.php
+++ app/Pegawai.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Pegawai extends Model
 {
     protected $table ='pegawai';
+    public function roles(){
+        return $this->belongsToMany('App\Pegawai','userkegiatan','nip','role_id');
+    }
 }
